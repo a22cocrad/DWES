8c039fafc69dc3c1c2c2969fcf5fc426e2eb11ef
Adrian <a22cocrad@iesgrancapitan.org>
2023-11-12 03:19:40 +0100

    actualizo estilo

--- Unidad_3/RA3_IT2_ACT1/Autoescuela/procesar.php
+++ Unidad_3/RA3_IT2_ACT1/Autoescuela/procesar.php
@@ -21,6 +21,7 @@ if (!isset($_POST['submit'])) {
     $test_number = $_POST["submit"];
     $questions = $aTests[$test_number - 1]["Preguntas"];
     $answers = $aTests[$test_number - 1]["Corrector"];
+    $approved = 0;
     $route_img = "./dir_img_test" . $test_number . "/img";
     echo "<form action='inicio.php' method='post'>";
 
@@ -30,21 +31,28 @@ if (!isset($_POST['submit'])) {
         echo '<img src="' . (file_exists($route_img . ($id_pregunta + 1) . ".jpg") ? $route_img . ($id_pregunta + 1) . ".jpg" : 'dir_img_test1/notfound.jpg') . '"> <br>';
         for ($j = 0; $j < 3; $j++) {
             echo '<label>';
-            
-            if ($_POST["respuesta" . $id_pregunta] == $questions[$id_pregunta]["respuestas"][$j][0] && $_POST["respuesta" . $id_pregunta] == $answers[$id_pregunta]) {
-                echo '<input checked type="radio" ><span class="correct">' . ($questions[$id_pregunta]["respuestas"][$j]) .  '</span></input> <br>';
+            if (($questions[$id_pregunta]['respuestas'][$j][0] == $answers[$id_pregunta]) && ($answers[$id_pregunta] == $_POST["respuesta" . $id_pregunta])) {
+                echo '<input checked disabled type="radio" ><span class="correct">✔️' . ($questions[$id_pregunta]["respuestas"][$j]) .  '</span></input> <br>';
+                $approved++;
+            } elseif (($questions[$id_pregunta]['respuestas'][$j][0] == $answers[$id_pregunta]) && ($answers[$id_pregunta] != $_POST["respuesta" . $id_pregunta])) {
+                echo '<input disabled type="radio" ><span class="correct">❌' . ($questions[$id_pregunta]["respuestas"][$j]) .  '</span></input> <br>';
+            } elseif ($_POST["respuesta" . $id_pregunta] == $questions[$id_pregunta]['respuestas'][$j][0]) {
+                echo '<input checked disabled type="radio"><span class="incorrect">❌' . ($questions[$id_pregunta]["respuestas"][$j]) .  '</span></input> <br>';
             } else {
-                if ($_POST["respuesta" . $id_pregunta] == $questions[$id_pregunta]["respuestas"][$j][0] && $_POST["respuesta" . $id_pregunta] != $answers[$id_pregunta]) {
-                    echo '<input  checked type="radio" ><span class="incorrect">' . ($questions[$id_pregunta]["respuestas"][$j]) .  '</span></input> <br>';
-                } else {
-                    echo '<input  disabled type="radio"><span class="incorrect">' . ($questions[$id_pregunta]["respuestas"][$j]) .  '</span></input> <br>';
-                }
+                echo '<input  disabled type="radio"><span class="incorrect">❌' . ($questions[$id_pregunta]["respuestas"][$j]) .  '</span></input> <br>';
             }
             echo '</label>';
         }
         echo '</div>';
     }
-    echo '<button name="submit">Volver al inicio</button>';
+    if ($approved >= 5) {
+        echo '✔️ ¡Enhorabuena! Has aprobado.✔️ <br>';
+        echo 'Número de aciertos: '. $approved;
+    } else {
+        echo '❌ ¡Lo siento! La próxima vez será. ❌<br>';
+        echo 'Número de fallos: '. 10-$approved;
+    }
+    echo '<br><br><button name="submit">Volver al inicio</button>';
 
     echo '</form>';
 
